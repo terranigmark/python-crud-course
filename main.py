f94371ca1783aeb05e1d58497404161830c6ae75
Hector Vega <linnk99@gmail.com>
2020-12-19 00:44:23 -0500

    Update client functionality added

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ def list_clients():
     global clients
     print(clients)
 
+
+def update_client(client_name, update_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', update_client_name + ',')
+    else:
+        print('Client is not in clients list.')
+
+
 def _add_comma():
     global clients
     clients += ','
@@ -25,18 +35,28 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+def _get_client_name():
+    return input('What is the client name?: ')
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
-    if command.upper() == 'C':
-        client_name = input('What is the client name?: ')
+    if command == 'C':
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
-    elif command.upper() == 'D':
+    elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the update client name?: ')
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print('Invalid command')
