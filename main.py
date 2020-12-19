6696044de10dc8890ca974f60dad1b5306138c4c
Hector Vega <linnk99@gmail.com>
2020-12-19 01:10:55 -0500

    Search functionality added

--- main.py
+++ main.py
@@ -22,7 +22,26 @@ def update_client(client_name, update_client_name):
     if client_name in clients:
         clients = clients.replace(client_name + ',', update_client_name + ',')
     else:
-        print('Client is not in clients list.')
+        _client_not_found()
+
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+    else:
+        _client_not_found()
+
+
+def search_client(client_name):
+    clients_list = clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
 
 
 def _add_comma():
@@ -35,11 +54,16 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do?')
     print('[C]reate client')
+    print('[L]ist  clients')
     print('[U]pdate client')
     print('[D]elete client')
+    print('[S]earch client')
 
 def _get_client_name():
     return input('What is the client name?: ')
+
+def _client_not_found():
+    return print('Client not found in client\'s list.')
 
 if __name__ == '__main__':
     _print_welcome()
@@ -52,11 +76,23 @@ if __name__ == '__main__':
         create_client(client_name)
         list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
     elif command == 'U':
         client_name = _get_client_name()
         updated_client_name = input('What is the update client name?: ')
         update_client(client_name, updated_client_name)
+        list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print(f'The client {client_name} is in the client\'s list')
+        else:
+            _client_not_found()
+    elif command == 'L':
         list_clients()
     else:
         print('Invalid command')
